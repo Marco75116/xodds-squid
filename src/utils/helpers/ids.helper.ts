bc96514bdaa6abd376b1a60709f633d916359528
Marco75116 <ourradourm@gmail.com>
2026-04-01 15:51:14 +0200

    chore: remove dead code

--- src/utils/helpers/ids.helper.ts
+++ src/utils/helpers/ids.helper.ts
@@ -1,9 +1,5 @@
 import { config } from "../../main";
 
-export const getTransferId = (logId: string) => {
-  return `${config.chainId}-${logId}`;
-};
-
 export const getAccountId = (logId: string) => {
   return `${config.chainId}-${logId}`;
 };
